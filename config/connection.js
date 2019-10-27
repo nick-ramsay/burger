21a9d893ad39b89213d3083348a30c269a3232a7
Nick Ramsay <NickRamsay1989@gmail.com>
2019-10-27 17:33:33 +1100

    Created db connection, add dotenv to hide keys

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+
+require("dotenv").config();
+
+const keys = require("./keys.js");
+const mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: keys.mysql.port,
+    user: "root",
+    password: keys.mysql.pw,
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+
+module.exports = connection;
