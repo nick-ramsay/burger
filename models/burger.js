5a5cb1adaf0814fb24e3d9d7c4003b76a13add93
Nick Ramsay <NickRamsay1989@gmail.com>
2019-10-27 18:49:30 +1100

    Adding views and controller starting files

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+const orm = require("../config/orm.js");
+
+orm.selectAll("burgers");
+orm.insertOne("burgers","burger_name",/*"newBurgerName_here"*/);
+orm.updateOne("burgers",/*"updateColumn"*/,/*"newValue"*/,/*"targetColumn"*/,/*targetValue*/);
+
+modules.export = orm.updateOne;
+modules.export = orm.insertOne;
+modules.export = updateOne;
