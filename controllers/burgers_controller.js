097ebcd190f95899c63a68ce7ea19d48e3bbb2c2
Nick Ramsay <NickRamsay1989@gmail.com>
2019-10-28 21:12:30 +1100

    Committing progress on some incomplete files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,10 @@
 const express = require("express");
+const router = express.Router();
+
 const burger = require("../models/burger.js");
+
+router.get("/", function(req,res) {
+    console.log("Testing get...");
+});
+
+module.exports = router;
